31ebb8d63a0070172176c26fba0deddcd17c06e9
wubz <wubingzhao@richinfo.cn>
2021-01-27 23:26:18 +0800

    planning表结构设计..

--- model/Planning.go
+++ model/Planning.go
@@ -6,6 +6,7 @@ type Planning struct {
 	Deadline  string //截止日期
 	Period    string //周期
 	CreatedAt string //规划创建时间
+	Creator   int    //创建人
 	Remarks   string //备注
-	Status    int    //状态
+	Status    int    //状态 0：待完成 1.已完成
 }
